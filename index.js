3868b72b86eb6c13fadb4ef4066b86449830ce51
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-19 04:11:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (ride <= 400) {
+    return `This one is on me!` 
+  } else if (ride >2000 && ride<2500){
+    return `I will gladly take your thirty bucks.`
+  }else if (ride > 2500){
+    return `No can do.`
+  }
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2551);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === "NYC"? "Ok, sounds good.": "No go."
 }
+// ternaryCheckCity("NYC");
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case tip = "generous":
+      return `Thank you so much.`;
+      break;
+    case tip = "not as generous":
+      return `Thank you.`;
+      break;
+    default:
+      return "Bye."   
+  }   
+};
+switchOnCharmFromTip("generous")
+switchOnCharmFromTip("not as generous")
+switchOnCharmFromTip(tip);
